d174d1ccc2798466e2745c0820a1b76bcf1ee486
7mochi <flyingcatdm@gmail.com>
2023-09-26 18:34:54 -0500

    chore: Remove make and only use yarn

--- bin/start-dev.sh
+++ bin/start-dev.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Cargando variables de entorno desde .env"
+set -a
+. ./.env
+set +a
+
 # Verificando la conexion a la base de datos MySQL
 bin/wait-for-it.sh $DB_HOST:$DB_PORT
 
